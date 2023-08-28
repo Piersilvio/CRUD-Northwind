a174965a07328b1f996835853207b25231713312
Piersilvio <piersilvio.spicoli@alten.it>
2023-08-28 14:36:53 +0200

    aggiunta repository in Product

--- DBLayer/DAO/RepositoryImp/ProductRepository.cs
+++ DBLayer/DAO/RepositoryImp/ProductRepository.cs
@@ -11,12 +11,11 @@ using System.Threading.Tasks;
 
 namespace DBLayer.DAO.DAOImpl
 {
-    public class ProductRepository : IRepositoryProduct
+    public class ProductRepository : Repository<Product>, IRepositoryProduct
     {
         private readonly NorthwindDefContext context;
 
-        //iniettato per DI 
-        public ProductRepository(NorthwindDefContext dbContext)
+        public ProductRepository(NorthwindDefContext dbContext) : base(dbContext)
         {
             context = dbContext;
         }
@@ -28,55 +27,6 @@ namespace DBLayer.DAO.DAOImpl
                 return await context.Product.Where(e => e.ProductName == name).ToListAsync();
             });
         } 
-
-        public async Task<Product> Create(Product entity)
-        {
-            return await ExceptionHandlerClass.HandleExceptions<Product>(async () =>
-            {
-                context.Product.Add(entity);
-                await context.SaveChangesAsync();
-
-                return entity;
-            });
-        }
-
-        public async Task<Product> Get(int id)
-        {
-            return await ExceptionHandlerClass.HandleExceptions<Product>(async () =>
-            {
-                Product? product = await context.Product.Where(e => e.ProductID == id)
-                .FirstOrDefaultAsync(e => e.ProductID == id);
-
-                return product;
-            });
-        }
-
-        public async Task<Product> Update(Product p)
-        {
-            return await ExceptionHandlerClass.HandleExceptions<Product>(async () =>
-            {
-                context.Product.Update(p);
-                await context.SaveChangesAsync();
-
-                return p;
-            });
-        }
-
-        public async Task<bool> Delete(Product p)
-        {
-            return await ExceptionHandlerClass.HandleExceptions<bool>(async () =>
-            {
-                bool isDeleted = false;
-
-                context.Product.Remove(p);
-                if (await context.SaveChangesAsync() > 0)
-                {
-                    isDeleted = true;
-                }
-
-                return isDeleted;
-            });
-        }
 
         public async Task<bool> Exists(int ProductID)
             => await context.Product.AnyAsync(o =>  o.ProductID == ProductID);
